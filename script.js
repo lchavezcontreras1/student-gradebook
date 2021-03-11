03a9143876cbfc1ec0ade7c8cc7a40fccf9f2d57
Lesli Chavez Contreras <lchavezcontreras1@my.wctc.edu>
2021-03-10 23:36:47 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+    $("#form-button").click(addObject);
+    addRow()
+    $("button#by-name").click(sortByName);
+    $("button#by-grade").click(sortByGrade);
+});
+let student = [
+    {name: "Sally", grade: 56},
+    {name: "Jill", grade: 80},
+    {name: "Ted", grade: 62},
+    {name: "Jim", grade: 82},
+    {name: "Sue", grade: 74},
+]
+
+function addRow() {
+    let theTable = $("tbody");
+    theTable.empty();
+    const start = 0;
+    const stop = student.length;
+    for (let i = start; i < stop; i++) {
+        let anItem = student[i];
+        let tr = $("<tr>");
+        theTable.append(tr);
+
+        let nameCell = $("<td>");
+        tr.append(nameCell);
+        nameCell.text(anItem.name);
+
+        let gradeCell = $("<td>");
+        tr.append(gradeCell);
+        gradeCell.text(`${anItem.grade}%`);
+    }
+}
+
+function addObject(e) {
+    e.preventDefault();
+    let name = $("select#fullname").val();
+    let grade = parseFloat($("input#grade").val()) + "%";
+    let newObject = {
+        name: name,
+        grade: grade
+    };
+    student.push(newObject);
+    addRow();
+}
+
+function sortByName(){
+    function byName(a, b) {
+        if (a.name < b.name)
+            return -1;
+        else if (a.name > b.name)
+            return 1;
+        else
+            return 0;
+    }
+    student.sort(byName);
+}
+
+function sortByGrade(){
+    function byGrade(a, b) {
+        if (a.grade < b.grade)
+            return -1;
+        else if (a.grade > b.grade)
+            return 1;
+        else
+            return 0;
+    }
+    student.sort(byGrade);
+}
+
+
+
+
+
